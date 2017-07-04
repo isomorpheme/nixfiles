0d2c4b40fd2b77a7811dff1aaf49d529433f6a73
Daan Rijks <daanrijks@gmail.com>
2017-07-04 13:35:28 +0200

    fish: set some environment variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,14 @@
 alias la "ls -a"
 
 abbr --add g git
+
+#
+# Environment variables
+#
+
+set -gx EDITOR "vim"
+set -gx GIT_EDITOR $EDITOR
+set -gx VISUAL "code"
+
+set -gx PATH "$HOME/.local/bin" $PATH
+set -gx PATH "$HOME/.cargo/bin" $PATH
