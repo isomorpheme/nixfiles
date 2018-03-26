f48e3647a0a761c3e43450dbbae0c09980973046
Daan Rijks <daanrijks@gmail.com>
2018-03-26 17:28:45 +0200

    fish: small fixes to the detect_package_manager function

--- fish/.config/fish/functions/detect_package_manager.fish
+++ fish/.config/fish/functions/detect_package_manager.fish
@@ -1,25 +1,37 @@
 function detect_package_manager --description "Detect the appropriate package manager to use."
-    switch (detect_distro)
+    set distro (detect_distro)
+    switch $distro
         case 'arch'
-            if type -q 'aurman'
-                set manager "aurman"
+            if type -q aurman
+                set managers 'aurman'
                 set do_sudo false
             else
-                set manager "pacman"
+                set managers 'pacman'
                 set do_sudo true
             end
 
         case 'ubuntu'
-            set manager "apt"
+            set managers 'apt'
             set do_sudo true
 
         case 'raspbian'
-            set manager "apt"
+            set managers 'apt'
+            set do_sudo true
+
+        case 'centos'
+            set managers 'yum' 'dnf'
             set do_sudo true
 
         case '*'
-            echo "Unknown distro! Package manager unset." 1>&2
+            echo "Unknown distro '$distro'. Package manager unset." 1>&2
             return 1
+    end
+
+    for m in $managers
+        if type -q $m
+            set $manager $m
+            break
+        end
     end
 
     if not type -q $manager
