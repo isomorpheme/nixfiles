5444a4580c82bd9bc5c149900d00a7656f701e3b
Daan Rijks <daanrijks@pm.me>
2022-10-06 23:31:24 +0200

    git: move to home-manager

--- home.nix
+++ home.nix
@@ -3,8 +3,53 @@
 {
   home.username = "daan";
   home.homeDirectory = "/home/daan";
-
   home.stateVersion = "22.05";
 
-  programs.home-manager.enable = true;
+  home.packages = [
+    pkgs.git-machete
+  ];
+
+  programs.home-manager = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+
+    userName = "Daan Rijks";
+    userEmail = "daanrijks@pm.me";
+
+    aliases = {
+      a = "add";
+      c = "commit";
+      co = "checkout";
+      sw = "switch";
+      ps = "push";
+      pl = "pull";
+      s = "status";
+    };
+
+    extraConfig = {
+      color.ui = true;
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = true;
+      };
+
+      pull.rebase = true;
+
+      merge.conflictstyle = "diff3";
+
+      rerere.enabled = true;
+
+      submodule.recurse = true;
+    };
+
+    lfs.enable = true;
+  };
+
+  programs.gh = {
+    enable = true;
+  };
 }
