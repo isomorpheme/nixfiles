eef2b4dad54be33aa3ca59b4542150e8c1ae254b
Daan Rijks <daanrijks@pm.me>
2022-10-08 15:45:37 +0200

    fish: add plugin 'done'

--- home.nix
+++ home.nix
@@ -92,6 +92,16 @@
       g = "git";
       hm = "home-manager";
     };
+
+    plugins = [{
+      name = "done";
+      src = pkgs.fetchFromGitHub {
+        owner = "franciscolourenco";
+        repo = "done";
+        rev = "d6abb267bb3fb7e987a9352bc43dcdb67bac9f06";
+        sha256 = "6oeyN9ngXWvps1c5QAUjlyPDQwRWAoxBiVTNmZ4sG8E=";
+      };
+    }];
   };
 
   programs.starship = {
