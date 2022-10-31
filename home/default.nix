a0cbe1c6c0c16b7d7db8c37c4d168bd968a1066b
Daan Rijks <daanrijks@pm.me>
2022-10-31 22:35:04 +0100

    home.sessionVariables: remove [LD_]LIBRARY_PATH

--- home/default.nix
+++ home/default.nix
@@ -15,11 +15,6 @@
   home.sessionVariables = {
     EDITOR = "vim";
     VISUAL = "code --wait";
-
-    # Make ~/.local act like an extension to /usr/local.
-    # FUTURE: This will get unnecessary as I nixify more stuff.
-    LIBRARY_PATH = "$HOME/.local/lib:$LIBRARY_PATH";
-    LD_LIBRARY_PATH = "$HOME/.local/lib:$LD_LIBRARY_PATH";
   };
 
   programs.home-manager = {
