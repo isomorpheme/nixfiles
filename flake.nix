b423fc832fef479c66c68e509bfaeedee392c00d
Daan Rijks <daanrijks@pm.me>
2022-11-05 22:57:52 +0100

    nixos: add initial configuration

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
+  outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.lega3yPackages.${system};
     in
     {
+      nixosConfigurations.Ada = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [ ./hosts/Ada/configuration.nix ];
+      };
+
       homeConfigurations.daan = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        pkgs = nixpkgs-unstable.legacyPackages.${system};
 
         modules = [ ./home ];
 
