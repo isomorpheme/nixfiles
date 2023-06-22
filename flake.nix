a94a55be2a1f3f48908f3ca2cfa80bf10d19cbf1
Daan Rijks <daanrijks@pm.me>
2023-06-22 13:18:03 +0200

    flake.nix: bump to nixpkgs and home-manager 23.05

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
