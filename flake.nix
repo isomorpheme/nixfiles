145bc41fd220859604e450473c88fa45b34b4fdf
Daan Rijks <daan.rijks@channable.com>
2024-08-08 11:19:05 +0200

    flake.nix: Bump nixpkgs to latest stable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
