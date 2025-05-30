4657d5e559ae5f85641ea6c44065096e2c777442
Daan Rijks <daanrijks@pm.me>
2025-05-31 01:01:45 +0200

    Put GHCi config in XDG_CONFIG_HOME
    It is now the far future!

--- modules/home-manager/dev.nix
+++ modules/home-manager/dev.nix
@@ -5,10 +5,8 @@
     pkgs.elan
   ];
 
-  # FUTURE: GHC 9.2 started looking in XDG_CONFIG_HOME, so put this there
-  # instead... in the far future when I don't have to use older versions
-  # anymore, hopefully.
-  home.file.".ghc/ghci.conf" = {
+  xdg.configFile."ghci.conf" = {
+    target = "ghc/ghci.conf";
     text = ''
       :set prompt "\ESC[1;35mλ> \ESC[m"
       :set prompt-cont "\ESC[1;35m > \ESC[m"
