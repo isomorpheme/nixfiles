f99b9cd7b8f91d64d6972d2cbc1f3b4cf4b69324
Daan Rijks <daanrijks@pm.me>
2025-05-31 00:54:49 +0200

    Merge common/ into parent dir

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,53 @@
+{ pkgs, flake, ... }:
 {
   imports = [
-    # TODO: Remove this level of indirection.
-    ./common
+    ./dev.nix
+    ./fish.nix
+    ./git.nix
+    ./utils.nix
+    ./vim.nix
+    ./zed.nix
   ];
+
+  nix.registry = {
+    # Add a flake registry entry pointing to the nixpkgs pinned in `flake.lock`,
+    # so that commands like `nix run` don't redownload the latest
+    # nixpkgs-unstable every time, and so that it's in sync with the nixpkgs
+    # used by home-manager.
+    nixpkgs = {
+      from = { id = "nixpkgs"; type = "indirect"; };
+      flake = flake.inputs.nixpkgs;
+    };
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  home.sessionPath = [
+    "$HOME/.local/bin"
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    VISUAL = "zed --wait";
+  };
+
+  home.packages = [
+    # Dev-related, but helps with Nix stuff in general so it goes here.
+    pkgs.cachix
+    pkgs.nixd
+    pkgs.nil
+
+    # Fonts
+    pkgs.jetbrains-mono
+  ];
+
+  fonts.fontconfig = {
+    enable = true;
+  };
+
+  programs.home-manager = {
+    enable = true;
+  };
 }
