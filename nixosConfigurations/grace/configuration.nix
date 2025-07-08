847c8bdc3d2989effbee5092ae5e046107c8101d
Daan Rijks <daanrijks@pm.me>
2025-07-08 22:42:37 +0200

    Add 1.1.1.1 as the default nameserver

--- nixosConfigurations/grace/configuration.nix
+++ nixosConfigurations/grace/configuration.nix
@@ -25,7 +25,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    insertNameservers = [ "1.1.1.1" ];
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
