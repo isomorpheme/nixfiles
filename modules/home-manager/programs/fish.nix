4dda6fa0cf70ef805427194fcd07e4acb44dec3a
Daan Rijks <daanrijks@pm.me>
2025-07-10 11:27:58 +0200

    Revert "Always switch to the fish version from $PATH"
    This reverts commit dd31818950b6acb169670b141313a5e721629a4a.
    
    It seems to mess with other programs that invoke fish to run a command.
    :(

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -3,14 +3,6 @@
   programs.fish = {
     enable = true;
 
-    shellInit = ''
-      # If we're not running as the fish from $PATH, we want to switch to that.
-      # (For example, because $PATH has a newer version.)
-      if test (status fish-path) != (readlink -f (which fish))
-          exec fish
-      end
-    '';
-
     shellAbbrs = {
       hm = "home-manager";
       nr = "nixos-rebuild";
