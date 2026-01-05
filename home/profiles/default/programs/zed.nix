5cbd78f5d027d14ac6ccb73b66b5a7cb5e78ba00
isomorpheme <iso@morpheme.tech>
2026-01-05 17:29:18 +0100

    Fix clashing values for $VISUAL

--- home/profiles/default/programs/zed.nix
+++ home/profiles/default/programs/zed.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 # Normally I have a mutable Zed install (both the executable itself and the config) but
 # there is some related configuration that is helpful.
 {
@@ -8,6 +9,6 @@
   };
 
   home.sessionVariables = {
-    VISUAL = "zed --wait";
+    VISUAL = lib.mkDefault "zed --wait";
   };
 }
