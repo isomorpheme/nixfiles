380d1c0d8c8070273fe4e48546da2e18ae4710aa
isomorpheme <iso@morpheme.tech>
2026-02-03 22:00:39 +0100

    Transmission: downgrade to 4.0.5

--- nixos/configurations/grace/nixarr.nix
+++ nixos/configurations/grace/nixarr.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   nixarr = {
     enable = true;
@@ -11,6 +12,22 @@
 
     transmission = {
       enable = true;
+      # Transmission 4.0.6 has a race condition where it may spam trackers with a lot of
+      # announce requests, leading to many trackers banning this version. Downgrading
+      # solves the issue for now.
+      #
+      # TODO: Once nixpkgs has Transmission at 4.1, this can be removed.
+      # See: https://github.com/NixOS/nixpkgs/pull/485318
+      package = pkgs.transmission_4.overrideAttrs (_: finalAttrs: {
+        version = "4.0.5";
+        src = pkgs.fetchFromGitHub {
+          owner = "transmission";
+          repo = "transmission";
+          rev = finalAttrs.version;
+          hash = "sha256-KBXvBFgrJ3njIoXrxHbHHLsiocwfd7Eba/GNI8uZA38=";
+          fetchSubmodules = true;
+        };
+      });
       flood.enable = true;
       extraSettings = {
         "ratio_limit" = 2.0;
